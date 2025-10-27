6939454b36a46fbdd80c0ef19e7f0976ee012ebc
brandony16 <108242812+brandony16@users.noreply.github.com>
2025-10-27 16:51:28 -0400

    Finish header layout

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,20 @@ import AuthButtons from "./AuthButtons.jsx";
 import Searchbar from "./Searchbar.jsx";
 
 const Header = () => {
+  const headerClick = () => {
+    // TODO: ROUTE TO HOME PAGE
+    console.log("Routing to home page");
+  };
   return (
     <div
       id="header"
       className="fixed top-0 left-0 w-screen flex items-center justify-between bg-gray-900 text-white px-8 py-4 w-100vw"
     >
-      <div id="title" className="flex items-center gap-3">
+      <div
+        id="title"
+        className="flex items-center gap-3 cursor-pointer"
+        onClick={headerClick}
+      >
         <img src={logo} alt="" className="h-10 w-10" />
         <h1 className="text-2xl font-semibold">Bits & Breakpoints</h1>
       </div>
--- src/components/header/Searchbar.jsx
+++ src/components/header/Searchbar.jsx
@@ -14,7 +14,6 @@ const Searchbar = () => {
       onSubmit={handleSubmit}
       className="flex items-center bg-gray-100 dark:bg-gray-800 rounded-full px-4 py-2 w-full max-w-md shadow-sm focus-within:ring-2 focus-within:ring-blue-500"
     >
-      <Search className="text-gray-500 dark:text-gray-400 mr-2" size={20} />
       <input
         type="text"
         placeholder="Search articles..."
@@ -22,6 +21,13 @@ const Searchbar = () => {
         value={query}
         onChange={(e) => setQuery(e.target.value)}
       />
+      <button
+        type="submit"
+        id="searchSubmit"
+        className="rounded-full cursor-pointer ml-2 hover:scale-1.02 transition-transform duration-200 transform hover:scale-110 active:scale-95"
+      >
+        <Search className="text-gray-500 dark:text-gray-400" size={20} />
+      </button>
     </form>
   );
 };
